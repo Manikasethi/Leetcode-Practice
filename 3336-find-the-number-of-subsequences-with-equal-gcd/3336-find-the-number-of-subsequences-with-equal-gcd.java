30632187d102a6ca1372892450f3f612d5fa0718
Manika Sethi <114850349+Manikasethi@users.noreply.github.com>
2026-07-14 23:13:31 +0530

    Time: 188 ms (69.44%), Space: 47.1 MB (88.89%) - LeetHub

---
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.java
@@ -0,0 +1,54 @@
+class Solution {
+    private static final int MOD = 1_000_000_007;
+
+    public int subsequencePairCount(int[] nums) {
+        int max = 0;
+        for (int x : nums) {
+            max = Math.max(max, x);
+        }
+
+        int[][] dp = new int[max + 1][max + 1];
+        dp[0][0] = 1;
+
+        for (int x : nums) {
+            int[][] next = new int[max + 1][max + 1];
+
+            // Skip current element
+            for (int i = 0; i <= max; i++) {
+                System.arraycopy(dp[i], 0, next[i], 0, max + 1);
+            }
+
+            for (int g1 = 0; g1 <= max; g1++) {
+                for (int g2 = 0; g2 <= max; g2++) {
+                    if (dp[g1][g2] == 0) continue;
+
+                    // Put current element in seq1
+                    int ng1 = (g1 == 0) ? x : gcd(g1, x);
+                    next[ng1][g2] = (int) ((next[ng1][g2] + (long) dp[g1][g2]) % MOD);
+
+                    // Put current element in seq2
+                    int ng2 = (g2 == 0) ? x : gcd(g2, x);
+                    next[g1][ng2] = (int) ((next[g1][ng2] + (long) dp[g1][g2]) % MOD);
+                }
+            }
+
+            dp = next;
+        }
+
+        long ans = 0;
+        for (int g = 1; g <= max; g++) {
+            ans = (ans + dp[g][g]) % MOD;
+        }
+
+        return (int) ans;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+}
