fc0116ebf8290bb6db3a2ecc90e871c35ed7113e
Manika Sethi <114850349+Manikasethi@users.noreply.github.com>
2026-08-06 00:58:10 +0530

    Time: 53 ms (93.27%), Space: 274.6 MB (93.27%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] edge : invocations) {
+            graph.get(edge[0]).add(edge[1]);
+        }
+
+        // Find all suspicious methods (reachable from k)
+        boolean[] suspicious = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(k);
+        suspicious[k] = true;
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+
+            for (int v : graph.get(u)) {
+                if (!suspicious[v]) {
+                    suspicious[v] = true;
+                    queue.offer(v);
+                }
+            }
+        }
+
+        // Check if any outside method invokes a suspicious method
+        for (int[] edge : invocations) {
+            int u = edge[0], v = edge[1];
+            if (!suspicious[u] && suspicious[v]) {
+                List<Integer> ans = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    ans.add(i);
+                }
+                return ans;
+            }
+        }
+
+        // Return remaining methods
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
